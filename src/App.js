5c03426f518d4afb5bc11fc56a9cca21d84ed779
Oumaima Ben Hamuda <oumaimabenhamouda18@gmail.com>
2022-02-10 21:08:42 +0100

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
-
+import Menu from './components/menu_component';
 function App() {
   return (
     <div className="App">
       <Navbar dark color="primary">
       <div className="container">
         <NavbarBrand href="/">Idk i'm confused</NavbarBrand>
-
       </div>
       </Navbar>
-     
+      <Menu/>
     </div>
   );
 }
